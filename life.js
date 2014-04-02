d5b41757bdd4c24a3f917d37b16f442875cb29f6
Jonatan Juárez <jonatan.ijp@gmail.com>
2014-04-02 13:07:49 -0600

    Fixed indentation

--- life.js
+++ life.js
@@ -2,12 +2,12 @@ var interval;
 
 var life = {
   board: [],
-	interval: 200,
-	dead: 0,
-	live: 0,
-	gen: 0,
+  interval: 200,
+  dead: 0,
+  live: 0,
+  gen: 0,
 
-	cleanBoard: function(){
+  cleanBoard: function(){
     var board = [];
 
     _(20).times(function(x){
@@ -17,81 +17,81 @@ var life = {
       });
     });
 
-		return board;
-	},
+    return board;
+  },
 
-	refresh: function(){
-		//Draw board
-		var html    = "";
-		var counter = 0;
-		this.dead   = 0;
-		this.live   = 0;
+  refresh: function(){
+    //Draw board
+    var html    = "";
+    var counter = 0;
+    this.dead   = 0;
+    this.live   = 0;
 
     _.each(this.board, function(row){
       _.each(row, function(cell){
-				html += "<li id='" + counter + "' class='cell "+ (cell == 0 ? "dead" : "live") + "'></li>";
-				counter++;
+        html += "<li id='" + counter + "' class='cell "+ (cell == 0 ? "dead" : "live") + "'></li>";
+        counter++;
 
         this.live += cell;
       });
     });
 
     this.dead = counter - this.live;
-		$("#board").html(html);
-		$(".cell").click(function(){
-			life.cellClick($(this).attr('id'));
-		});
-		this.refreshPanel();
-	},
+    $("#board").html(html);
+    $(".cell").click(function(){
+      life.cellClick($(this).attr('id'));
+    });
+    this.refreshPanel();
+  },
 
-	refreshPanel: function(){
-		$("#dead").html(this.dead);
-		$("#live").html(this.live);
-		$("#gen").html(this.gen);
-	},
+  refreshPanel: function(){
+    $("#dead").html(this.dead);
+    $("#live").html(this.live);
+    $("#gen").html(this.gen);
+  },
 
-	cellClick: function(id){
-		//Change a dead cell to live
-		$("#" + id).toggleClass("dead");
-		$("#" + id).toggleClass("live");
+  cellClick: function(id){
+    //Change a dead cell to live
+    $("#" + id).toggleClass("dead");
+    $("#" + id).toggleClass("live");
 
-		this.board[Math.floor(id/20)][id%20] = this.board[Math.floor(id/20)][id%20] == 0 ? 1 : 0;
+    this.board[Math.floor(id/20)][id%20] = this.board[Math.floor(id/20)][id%20] == 0 ? 1 : 0;
 
-		if(this.board[Math.floor(id/20)][id%20] == 1){
-			this.dead--;
-			this.live++;
-		}else{
-			this.dead++;
-			this.live--;
-		}
+    if(this.board[Math.floor(id/20)][id%20] == 1){
+      this.dead--;
+      this.live++;
+    }else{
+      this.dead++;
+      this.live--;
+    }
 
-		this.refreshPanel();
-	},
+    this.refreshPanel();
+  },
 
-	init: function(){
-		$("#start").attr('disabled', true);
-		$("#stop").attr('disabled', false);
+  init: function(){
+    $("#start").attr('disabled', true);
+    $("#stop").attr('disabled', false);
 
-		this.process = setInterval(function(){
-			life.nextGen();
-		},this.interval);
-	},
+    this.process = setInterval(function(){
+      life.nextGen();
+    },this.interval);
+  },
 
-	stop: function(){
-		$("#stop").attr('disabled', true);
-		$("#start").attr('disabled', false);
+  stop: function(){
+    $("#stop").attr('disabled', true);
+    $("#start").attr('disabled', false);
 
-		clearInterval(this.process);
-	},
+    clearInterval(this.process);
+  },
 
-	nextGen: function(){
-		this.gen++;
+  nextGen: function(){
+    this.gen++;
     var cleanBoard = this.cleanBoard();
     var live = 0;
 
-		for (var i = 0; i < this.board.length; i++) {
-			for (var j = 0; j < this.board[i].length; j++) {
-				live = 0;
+    for (var i = 0; i < this.board.length; i++) {
+      for (var j = 0; j < this.board[i].length; j++) {
+        live = 0;
         //Check state of neighbour cells
         for (var x = i-1; x < i+2; x++){
           for (var y = j-1; y < j+2; y++){
@@ -102,24 +102,24 @@ var life = {
           }
         }
 
-				if(this.board[i][j]){
+        if(this.board[i][j]){
           //A live cell with 2 or 3 lives cell lives on the next gen
-					if(live == 2 || live == 3){
-						cleanBoard[i][j] = 1;
-					}
+          if(live == 2 || live == 3){
+            cleanBoard[i][j] = 1;
+          }
         }else{
           //A dead cell with exactly three live neighbours becomes a live cell
-					if(live == 3){
-						//Revive
-						cleanBoard[i][j] = 1;
-					}
+          if(live == 3){
+            //Revive
+            cleanBoard[i][j] = 1;
+          }
         }
-			}
-		}
+      }
+    }
 
     this.board = _.clone(cleanBoard);
-		this.refresh();
-	}
+    this.refresh();
+  }
 };
 
 
@@ -127,6 +127,6 @@ $(document).ready(function(){
 
   life.board = life.cleanBoard();
 
-	//Draw board
-	life.refresh();
+  //Draw board
+  life.refresh();
 });
