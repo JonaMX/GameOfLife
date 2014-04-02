fecc15a61c490c3b56a67689e29914dd7415acd5
Jonatan Juárez <jonatan.ijp@gmail.com>
2014-04-02 13:06:12 -0600

    Refactor

--- life.js
+++ life.js
@@ -1,100 +1,53 @@
 var interval;
 
 var life = {
-	tablero: [
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	],
-
-	tableroAux: [
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	],
-
-	interval: 200, 
+  board: [],
+	interval: 200,
 	dead: 0,
 	live: 0,
 	gen: 0,
 
-	cleanGrid: function(grid){
-		for (var i = 0; i < grid.length; i++) {
-			for (var j = 0; j < grid[i].length; j++) {
-				grid[i][j] = 0;
-			}
-		}
+	cleanBoard: function(){
+    var board = [];
 
-		return grid;
+    _(20).times(function(x){
+      board.push([]);
+      _(20).times(function(y){
+        board[x].push(0);
+      });
+    });
+
+		return board;
 	},
 
 	refresh: function(){
-		//Dibujar tablero
-		html = "";
-		contador = 0;
-		life.dead = 0;
-		life.live = 0;
+		//Draw board
+		var html    = "";
+		var counter = 0;
+		this.dead   = 0;
+		this.live   = 0;
 
-		for (var i = 0; i < life.tablero.length; i++) {
-			for (var j = 0; j < life.tablero[i].length; j++) {
-				status = life.tablero[i][j] == 0 ? "dead" : "live";
-				html += "<li id='" + contador + "' class='cell "+ status + "'></li>";
-				contador++;
+    _.each(this.board, function(row){
+      _.each(row, function(cell){
+				html += "<li id='" + counter + "' class='cell "+ (cell == 0 ? "dead" : "live") + "'></li>";
+				counter++;
 
-				if(life.tablero[i][j] == 1)
-					life.live++;
-				else
-					life.dead++;
-			};
-		};
+        this.live += cell;
+      });
+    });
 
-		$("#tablero").html(html);
-
+    this.dead = counter - this.live;
+		$("#board").html(html);
 		$(".cell").click(function(){
 			life.cellClick($(this).attr('id'));
 		});
-
-		life.refreshPanel();
+		this.refreshPanel();
 	},
 
 	refreshPanel: function(){
-		$("#dead").html(life.dead);
-		$("#live").html(life.live);
-		$("#gen").html(life.gen);
+		$("#dead").html(this.dead);
+		$("#live").html(this.live);
+		$("#gen").html(this.gen);
 	},
 
 	cellClick: function(id){
@@ -102,96 +55,78 @@ var life = {
 		$("#" + id).toggleClass("dead");
 		$("#" + id).toggleClass("live");
 
-		life.tablero[Math.floor(id/20)][id%20] = life.tablero[Math.floor(id/20)][id%20] == 0 ? 1 : 0;
+		this.board[Math.floor(id/20)][id%20] = this.board[Math.floor(id/20)][id%20] == 0 ? 1 : 0;
 
-		if(life.tablero[Math.floor(id/20)][id%20] == 1){
-			life.dead--;
-			life.live++;
+		if(this.board[Math.floor(id/20)][id%20] == 1){
+			this.dead--;
+			this.live++;
 		}else{
-			life.dead++;
-			life.live--;
+			this.dead++;
+			this.live--;
 		}
 
-		life.refreshPanel();
+		this.refreshPanel();
 	},
 
 	init: function(){
 		$("#start").attr('disabled', true);
 		$("#stop").attr('disabled', false);
 
-		interval = setInterval(function(){
+		this.process = setInterval(function(){
 			life.nextGen();
-		},life.interval);
+		},this.interval);
 	},
 
 	stop: function(){
 		$("#stop").attr('disabled', true);
 		$("#start").attr('disabled', false);
 
-		clearInterval(interval);
+		clearInterval(this.process);
 	},
 
 	nextGen: function(){
-		life.gen++;
-		life.tableroAux = life.cleanGrid(life.tableroAux);
+		this.gen++;
+    var cleanBoard = this.cleanBoard();
+    var live = 0;
 
-		for (var i = 0; i < life.tablero.length; i++) {
-			for (var j = 0; j < life.tablero[i].length; j++) {
-				vivas = 0;
+		for (var i = 0; i < this.board.length; i++) {
+			for (var j = 0; j < this.board[i].length; j++) {
+				live = 0;
+        //Check state of neighbour cells
+        for (var x = i-1; x < i+2; x++){
+          for (var y = j-1; y < j+2; y++){
+            //Check board limits
+            if(x >= 0 && x < this.board.length && y >= 0 &&  y < this.board[i].length && !(x == i && y == j)){
+              live = this.board[x][y] == 1 ? live + 1 : live;
+            }
+          }
+        }
 
-				//Una célula muerta con exactamente 3 células vecinas vivas "nace".
-				if(life.tablero[i][j] == 0){
-					//Comparar estado de celulas vecinas
-					for (var x = i-1; x < i+2; x++){
-						for (var y = j-1; y < j+2; y++){
-							//Comprobar limites de tablero y que no sea la misma celula
-							if(x >= 0 && x < life.tablero.length && y >= 0 &&  y < life.tablero[i].length && !(x == i && y == j)){
-								vivas = life.tablero[x][y] == 1 ? vivas + 1 : vivas;
-							}
-						}
+				if(this.board[i][j]){
+          //A live cell with 2 or 3 lives cell lives on the next gen
+					if(live == 2 || live == 3){
+						cleanBoard[i][j] = 1;
 					}
-
-					if(vivas == 3){
+        }else{
+          //A dead cell with exactly three live neighbours becomes a live cell
+					if(live == 3){
 						//Revive
-						life.tableroAux[i][j] = 1;
+						cleanBoard[i][j] = 1;
 					}
-				}
-
-				vivas = 0;
-				//Una célula muerta con exactamente 3 células vecinas vivas "nace".
-				if(life.tablero[i][j] == 1){
-					//Comparar estado de celulas vecinas
-					for (var x = i-1; x < i+2; x++){
-						for (var y = j-1; y < j+2; y++){
-							//Comprobar limites de tablero y que no sea la misma celula
-							if(x >= 0 && x < life.tablero.length && y >= 0 &&  y < life.tablero[i].length && !(x == i && y == j)){
-								vivas = life.tablero[x][y] == 1 ? vivas + 1 : vivas;
-							}
-						}
-					}
-
-					if(vivas >= 2 && vivas <= 3){
-						//Permanece viva
-						life.tableroAux[i][j] = 1;
-					}
-				}
+        }
 			}
 		}
 
-		for (var i = 0; i < life.tablero.length; i++) {
-			for (var j = 0; j < life.tablero[i].length; j++) {
-				life.tablero[i][j] = life.tableroAux[i][j];
-			}
-		}
-
-
-
-		life.refresh();
+    this.board = _.clone(cleanBoard);
+		this.refresh();
 	}
 };
 
 
 $(document).ready(function(){
-	//Dibujar tablero
+
+  life.board = life.cleanBoard();
+
+	//Draw board
 	life.refresh();
 });
